814c3f366cc4b0999f33dfb113262bc25d385351
CAMILO ANDRES MENDOZA ARRIETA <ca.mendoza968@sis.virtual.uniandes.edu.co>
2017-10-11 18:44:22 -0500

    Example Temperature API Spring

---
+++ src/main/java/com/camendoza94/temperatureapi/MeasurementRepository.java
@@ -0,0 +1,12 @@
+package com.camendoza94.temperatureapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
+public interface MeasurementRepository extends MongoRepository<Measurement, String> {
+    List<Measurement> findByTimestampBetween(@Param("dateInit") @DateTimeFormat(pattern="yyyy-MM-dd") Date dateInit, @Param("dateEnd") @DateTimeFormat(pattern="yyyy-MM-dd") Date dateEnd);
+}
